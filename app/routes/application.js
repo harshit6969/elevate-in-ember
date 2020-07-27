df78c70c9db03707b4573d1dcb448e8a51bcb42e
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-27 16:56:36 +0530

    Remove extra files

--- app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class FloorsRoute extends Route {
-  async model() {
-    let response = await fetch('/floor-setup.json');
-    let parsed = await response.json();
-    return parsed;
-  }
-}
