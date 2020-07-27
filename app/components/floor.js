0fbac690d2fbb4c57c1cd6ba4c20fc891cfbfbb5
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-07-27 08:28:14 +0530

    feat: add floor plan manager

--- app/components/floor.js
+++ app/components/floor.js
@@ -1,9 +1,17 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class PeopleListComponent extends Component {
-  @action
-  showPerson(person) {
-    alert(`The person's name is ${person}!`);
-  }
+export default class ApplicationComponent extends Component {
+	@service floorManager
+
+	@action handleUp(e) {
+		const floorNumber = parseInt(e.target.value, 10);
+		this.floorManager.updateLiftPosition('up', floorNumber);
+	}
+
+	@action handleDown(e) {
+		const floorNumber = parseInt(e.target.value, 10);
+		this.floorManager.updateLiftPosition('down', floorNumber);
+	}
 }
---
+++ app/services/floor-manager.js
@@ -0,0 +1,94 @@
+import Service from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+
+export default class FloorManagerService extends Service {
+  @tracked floorSetup = [
+    {
+      number: 5,
+      hasLiftA: false,
+      hasLiftB: false,
+      hasLiftC: true,
+      isLast: true,
+      isFirst: false,
+    },
+    {
+      number: 4,
+      hasLiftA: false,
+      hasLiftB: false,
+      hasLiftC: false,
+      isLast: false,
+      isFirst: false,
+    },
+    {
+      number: 3,
+      hasLiftA: false,
+      hasLiftB: true,
+      hasLiftC: false,
+      isLast: false,
+      isFirst: false,
+    },
+    {
+      number: 2,
+      hasLiftA: false,
+      hasLiftB: false,
+      hasLiftC: false,
+      isLast: false,
+      isFirst: false,
+    },
+    {
+      number: 1,
+      hasLiftA: true,
+      hasLiftB: false,
+      hasLiftC: false,
+      isLast: false,
+      isFirst: true,
+    },
+  ];
+
+  updateLiftPosition(movementType, floorNumber) {
+    const length = this.floorSetup.length;
+    const currentFloor = this.floorSetup.filter(
+      (floor) => floor.number === floorNumber
+    )[0];
+
+    const { hasLiftA, hasLiftB, hasLiftC } = currentFloor;
+
+    let nextFloor;
+
+    if (movementType === "up") {
+      if (currentFloor.isLast) {
+        alert("Where are you headed bruh!");
+        return;
+      }
+
+      nextFloor = this.floorSetup.filter(
+        (floor) => floor.number === floorNumber + 1
+      )[0];
+    } else {
+      if (currentFloor.isFirst) {
+        alert("Where are you headed bruh!");
+        return;
+      }
+
+      nextFloor = this.floorSetup.filter(
+        (floor) => floor.number === floorNumber - 1
+      )[0];
+    }
+
+    const nextFloorNewState = { ...nextFloor, hasLiftA, hasLiftB, hasLiftC };
+    const currentFloorNewState = {
+      ...currentFloor,
+      hasLiftA: hasLiftA === true ? !hasLiftA : hasLiftA,
+      hasLiftB: hasLiftB === true ? !hasLiftB : hasLiftB,
+      hasLiftC: hasLiftC === true ? !hasLiftC : hasLiftC,
+    };
+
+    const newFloorState =
+      movementType === "up"
+        ? [nextFloorNewState, currentFloorNewState]
+        : [currentFloorNewState, nextFloorNewState];
+
+    this.floorSetup.splice(length - floorNumber - 1, 2, ...newFloorState);
+    console.log(this.floorSetup);
+  }
+}
